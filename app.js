41a910863c320c5757162e5af22327198c802642
ceruse <dlswns020@naver.com>
2022-08-29 22:37:33 +0900

    test4

--- app.js
+++ app.js
@@ -19,11 +19,18 @@ const SalesRouter = require('./routes/Sales');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/build")));
 
+// 미들웨어 함수를 특정 경로에 등록
+app.use('/api', function(req, res) {
+    res.json({ greeting: 'Hello World' });
+});
+
+// 리액트 정적 파일 제공
+app.use(express.static(path.join(__dirname, "/build")));
+// 라우트 설정
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/build', 'index.html'));
-  });
+});
 
 // API
 app.use('/api/auth', authRouter);
