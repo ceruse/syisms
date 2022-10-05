19cbc18eafa9e23dabec98b8b7a7953092aab53d
ceruse <dlswns020@naver.com>
2022-10-05 23:27:12 +0900

    make it better

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b5ceb4ce213d42',
     port: '3306',
-    database: 'project',
-    password: 'project',
+    database: 'heroku_859b7c4d2d4e797',
+    password: '05965e3a',
    };
